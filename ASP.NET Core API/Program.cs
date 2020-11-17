1612766c126fbe630bb02a3922605676bdced5f4
Tomi <tomi-best@hotmail.com>
2020-11-17 13:27:30 +0200

    Used ASP.NET Core Api example instead of the blank file

--- ASP.NET Core API/Program.cs
+++ ASP.NET Core API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ASP.NET_Core_API
+namespace ASP.NET_Core_Api
 {
     public class Program
     {
